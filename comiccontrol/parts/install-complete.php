1c8e44db54f2086debd1217132a8593089929c04
Mr. Encyclopedia <44952717+mrencyc@users.noreply.github.com>
2023-06-13 10:22:53 -0400

    Add files via upload

--- comiccontrol/parts/install-complete.php
+++ comiccontrol/parts/install-complete.php
@@ -1,5 +1,5 @@
-<?php include('includes/install-header.php');  ?>
-
-<?=$ilang['complete']?>
-
+<?php include('includes/install-header.php');  ?>
+
+<?=$ilang['complete']?>
+
 <?php include('includes/install-footer.php'); ?>

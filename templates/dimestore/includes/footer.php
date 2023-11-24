b63a731767b781b34d95e382eafcd3e6385d8a41
Mr. Encyclopedia <44952717+mrencyc@users.noreply.github.com>
2023-11-24 18:17:43 -0500

    Delete templates/dimestore directory

--- templates/dimestore/includes/footer.php
+++
@@ -1,8 +0,0 @@
-    </main>
-</div>
-<footer id="cc-footer">
-<div>This site is powered by <a href="https://www.comicctrl.com/">ComicControl</a>.</div>
-<div><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This website and the art it contains are licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</div>
-</footer>
-</body>
-</html>

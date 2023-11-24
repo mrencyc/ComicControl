f4cf25d016f452e1d056cbb3d94d24b6bb7c7591
Mr. Encyclopedia <44952717+mrencyc@users.noreply.github.com>
2023-11-24 18:20:11 -0500

    Update page.php
    RSS update

--- templates/basic/page.php
+++ templates/basic/page.php
@@ -1,15 +1,29 @@
-<?php 
-//rss switch
-if($ccpage->subslug == "rss"){ 
-
-	include($ccsite->ccroot . 'parts/blog-rss.php');
-	
-}else{
-
-include('templates/basic/includes/header.php');
-
-$ccpage->module->display();
-
-include('templates/basic/includes/footer.php'); 
-
+<?php 
+
+//rss switch
+
+if($ccpage->subslug == "rss"){ 
+
+
+
+	include($ccsite->ccroot . 'parts/rss.php');
+
+	
+
+}else{
+
+
+
+include('templates/basic/includes/header.php');
+
+
+
+$ccpage->module->display();
+
+
+
+include('templates/basic/includes/footer.php'); 
+
+
+
 }?>
